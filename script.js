fe9118cae77bd7dd4dfb1b80bd72868fc0cb9da4
Gedoy26 <cgquilitorio@gmail.com>
2026-08-19 23:47:51 +0800

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded',()=> {
-  const cta = document.getElementById('cta');
-  if(cta){
-    cta.addEventListener('click',()=> {
-      alert('Thanks! Check the in-game event board for details.');
-    });
-  }
-  console.log('Cabal Sea Saga event page loaded');
-});
